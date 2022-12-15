f225dc2506e6229f0c8022ff1edbd5fb613c911f
aam <muh.muammar@gmail.com>
2022-12-15 11:45:35 +0800

    tambah file tsconfig sama example

---
+++ src/example.ts
@@ -0,0 +1,133 @@
+import { Boom } from '@hapi/boom'
+import P from 'pino'
+import makeWASocket, { AnyMessageContent, delay, DisconnectReason, fetchLatestBaileysVersion, isJidBroadcast, makeInMemoryStore, useMultiFileAuthState } from '@adiwajshing/baileys'
+
+const doReplies = !process.argv.includes('--no-reply')
+
+
+// start a connection
+const startSock = async() => {
+	const { state, saveCreds } = await useMultiFileAuthState('auth_info_baileys')
+	
+	// can be written out to a file & read from it
+	const store = makeInMemoryStore({})
+	// can be read from a file
+	store.readFromFile('./baileys_store.json')
+	// saves the state to a file every 10s
+	setInterval(() => {
+		store.writeToFile('./baileys_store.json')
+	}, 10_000)
+    
+	const sock = makeWASocket({
+		logger: P({ level: 'error' }),
+		printQRInTerminal: true,
+		auth: state,
+	})
+
+	// will listen from this socket
+	// the store can listen from a new socket once the current socket outlives its lifetime
+	store.bind(sock.ev)
+
+	const sendMessageWTyping = async(msg: AnyMessageContent, jid: string) => {
+		await sock.presenceSubscribe(jid)
+		await delay(500)
+
+		await sock.sendPresenceUpdate('composing', jid)
+		await delay(2000)
+
+		await sock.sendPresenceUpdate('paused', jid)
+
+		await sock.sendMessage(jid, msg)
+	}
+
+	// the process function lets you process all events that just occurred
+	// efficiently in a batch
+	sock.ev.process(
+		// events is a map for event name => event data
+		async(events) => {
+			// something about the connection changed
+			// maybe it closed, or we received all offline message or connection opened
+			if(events['connection.update']) {
+				const update = events['connection.update']
+				const { connection, lastDisconnect } = update
+				if(connection === 'close') {
+					// reconnect if not logged out
+					if((lastDisconnect?.error as Boom)?.output?.statusCode !== DisconnectReason.loggedOut) {
+						startSock()
+					} else {
+						console.log('Connection closed. You are logged out.')
+					}
+				}
+
+				console.log('connection update', update)
+			}
+
+			// credentials updated -- save them
+			if(events['creds.update']) {
+				await saveCreds()
+			}
+
+			if(events.call) {
+				console.log('recv call event', events.call)
+			}
+
+			// received a new message
+			if(events['messages.upsert']) {
+				const upsert = events['messages.upsert']
+				console.log('recv messages ', JSON.stringify(upsert, undefined, 2))
+
+				if(upsert.type === 'notify') {
+					for(const msg of upsert.messages) {
+						if(!msg.key.fromMe && doReplies) {
+							console.log('replying to', msg.key.remoteJid)
+							await sock!.readMessages([msg.key])
+							await sendMessageWTyping({ text: 'Hello there!' }, msg.key.remoteJid!)
+						}
+					}
+				}
+			}
+
+			// messages updated like status delivered, message deleted etc.
+			if(events['messages.update']) {
+				console.log(events['messages.update'])
+			}
+
+			if(events['message-receipt.update']) {
+				console.log(events['message-receipt.update'])
+			}
+
+			if(events['messages.reaction']) {
+				console.log(events['messages.reaction'])
+			}
+
+			if(events['presence.update']) {
+				console.log(events['presence.update'])
+			}
+
+			if(events['chats.update']) {
+				console.log(events['chats.update'])
+			}
+
+			if(events['contacts.update']) {
+				for(const contact of events['contacts.update']) {
+					if(typeof contact.imgUrl !== 'undefined') {
+						const newUrl = contact.imgUrl === null
+							? null
+							: await sock!.profilePictureUrl(contact.id!).catch(() => null)
+						console.log(
+							`contact ${contact.id} has a new profile pic: ${newUrl}`,
+						)
+					}
+				}
+			}
+
+			if(events['chats.delete']) {
+				console.log('chats deleted ', events['chats.delete'])
+			}
+		}
+	)
+
+	return sock
+}
+
+startSock()
